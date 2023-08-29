54141f9a88afc506a8f7abf78ccb0efcfdd89397
Programador6 <143215120+Programador6Comfenalco@users.noreply.github.com>
2023-08-29 01:12:13 -0500

    w

--- index.php
+++ index.php
@@ -9,27 +9,7 @@
 
 <body>
     <h1>hello</h1>
-    <?php
 
-    try {
-        // Ejecutamos el comando 
-        $output = shell_exec('java -version');
-
-        // Mostramos el resultado
-        echo "Salida del comando: <br>";
-        echo $output;
-
-        // Verificamos si java está instalado
-        if (strpos($output, 'java version') !== FALSE) {
-            echo "<br>Java está instalado!";
-        } else {
-            echo "<br>Java NO está instalado";
-        } //code...
-    } catch (\Throwable $th) {
-        echo "error de documento";
-    }
-
-    ?>
 </body>
 
 </html>
